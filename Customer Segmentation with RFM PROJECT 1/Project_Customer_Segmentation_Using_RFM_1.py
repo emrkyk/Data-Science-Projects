c7dbbb80d82d06ea907835db75d89e9ff574ec7a
Emre Kıyak <62489182+emrkyk@users.noreply.github.com>
2021-05-21 05:26:13 +0200

    Update Project_Customer_Segmentation_Using_RFM_1.py

--- Customer Segmentation with RFM PROJECT 1/Project_Customer_Segmentation_Using_RFM_1.py
+++ Customer Segmentation with RFM PROJECT 1/Project_Customer_Segmentation_Using_RFM_1.py
@@ -31,8 +31,7 @@ pd.set_option('display.max_columns', None)
 # pd.set_option('display.max_rows', None)
 # pd.set_option('display.float_format', lambda x: '%.5f' % x)
 
-data = pd.read_excel("Datasets/online_retail_II.xlsx",
-                    sheet_name="Year 2009-2010")
+data = pd.read_excel("Datasets/online_retail_II.xlsx", sheet_name="Year 2009-2010")
 
 df = data.copy()
 df.head()
@@ -80,11 +79,11 @@ df["InvoiceDate"].max()  # 2010-12-09
 
 today_date = dt.datetime(2010, 12, 11)  # reasonable to assume today's date as 2 or 1 days later than last date
 
-rfm = df.groupby('Customer ID').agg({'InvoiceDate': lambda date: (today_date - date.max()).days,
-                                     'Invoice': lambda num: len(num),
-                                     'TotalPrice': lambda TotalPrice: TotalPrice.sum()})
+rfm = df.groupby("Customer ID").agg({"InvoiceDate": lambda date: (today_date - date.max()).days,
+                                     "Invoice": lambda num: len(num),
+                                     "TotalPrice": lambda TotalPrice: TotalPrice.sum()})
 
-rfm.columns = ['Recency', 'Frequency', 'Monetary']
+rfm.columns = ["Recency", "Frequency", "Monetary"]
 
 rfm = rfm[(rfm["Monetary"] > 0) & (rfm["Frequency"] > 0)]
 rfm.head()
@@ -102,17 +101,17 @@ rfm.head()
 # =======================
 
 # Recency
-rfm["RecencyScore"] = pd.qcut(rfm['Recency'], 5, labels=[5, 4, 3, 2, 1])
+rfm["RecencyScore"] = pd.qcut(rfm["Recency"], 5, labels=[5, 4, 3, 2, 1])
 
 # Frecuency
-rfm["FrequencyScore"] = pd.qcut(rfm['Frequency'], 5, labels=[1, 2, 3, 4, 5])
+rfm["FrequencyScore"] = pd.qcut(rfm["Frequency"], 5, labels=[1, 2, 3, 4, 5])
 
 # Monetary
-rfm["MonetaryScore"] = pd.qcut(rfm['Monetary'], 5, labels=[1, 2, 3, 4, 5])
+rfm["MonetaryScore"] = pd.qcut(rfm["Monetary"], 5, labels=[1, 2, 3, 4, 5])
 
-rfm["RFM_SCORE"] = (rfm['RecencyScore'].astype(str) +
-                    rfm['FrequencyScore'].astype(str) +
-                    rfm['MonetaryScore'].astype(str))
+rfm["RFM_SCORE"] = (rfm["RecencyScore"].astype(str) +
+                    rfm["FrequencyScore"].astype(str) +
+                    rfm["MonetaryScore"].astype(str))
 
 rfm[rfm["RFM_SCORE"] == "555"].head()  # Champions group!!!
 
@@ -139,9 +138,9 @@ seg_map = {
 
 rfm
 
-rfm['Segment'] = rfm['RecencyScore'].astype(str) + rfm['FrequencyScore'].astype(str)
+rfm["Segment"] = rfm["RecencyScore"].astype(str) + rfm["FrequencyScore"].astype(str)
 
-rfm['Segment'] = rfm['Segment'].replace(seg_map, regex=True)  
+rfm["Segment"] = rfm["Segment"].replace(seg_map, regex=True)  
 
 df[["Customer ID"]].nunique()
 rfm[["Segment", "Recency", "Frequency", "Monetary"]].groupby("Segment").agg(["mean", "count"])
